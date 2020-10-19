626c8fdb897d713ba7041349800b5814752bb252
Diana Vafina <Dianochka.25@mail.ru>
2020-10-19 18:54:55 +0300

    Main.java added

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPriceWithPenny = 1021_00;
+        int Miles = ticketPriceWithPenny / 20 / 100;
+
+        System.out.println(Miles);
+
+
+    }
+}
